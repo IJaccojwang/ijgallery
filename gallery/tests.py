2d668a1a97cb2091f80ba0344d5a8f2c1e1bf5c9
Jaccojwang <danolago@gmail.com>
2019-05-10 14:58:39 +0300

    Fix isinstance test for image

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,28 +1,29 @@
 from django.test import TestCase
-from .models import images, location, category
+from .models import Image, location, category
 import datetime as dt
 
 
 class  ImageTestClass(TestCase):
     
     def setUp(self):
+        # Creating a new location and saving it
+        self.location = location(name = 'Testlocation')
+        self.location.save()
+
         # Creating a new editor and saving it
-        self.image= Image(image = "/images", name = "TestImage", description = "A image for testing",)
+        self.image= Image(image = "/images", name = "TestImage", description = "A image for testing", location =self.location )
         self.image.save_image()
         # Testing  instance
+    
+        # Creating a new category and saving it
+        self.category = category(name = 'Testcategory')
+        self.category.save()
+
+        self.image.categories.add(self.category)
+
     def test_instance(self):
         self.assertTrue(isinstance(self.image,Image))
 
-        # Creating a new category and saving it
-        self.category = tags(name = 'Testcategory')
-        self.category.save()
-
-        # Creating a new location and saving it
-        self.location = tags(name = 'Testlocation')
-        self.location.save()
-
-        self.image.location.add(self.location)
-        self.image.category.add(self.category)
 
     def tearDown(self):
         Image.objects.all().delete()
