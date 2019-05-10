d74bca02a7bf15690977a925ea77df414b10aa90
Jaccojwang <danolago@gmail.com>
2019-05-10 11:26:01 +0300

    Configure app views

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image
 
-# Create your views here.
+
+def home(request):
+    images = Image.all_images()
+    return render(request, 'home.html', {"images": images})
+
+def category(request, category):
+    images = Image.all_images()
+    return render(request, 'category.html', {"images": images})
+
+def location(request, location):
+    images = Image.filter_by_location(location)
+    return render(request, 'location.html', {"images": images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        searched = request.GET.get("image")
+        images = Image.search_image(searched)
+        message = f"{searched}"
+
+        return render(request, 'search.html', {"message":message,"images": images})
+    else:
+        message = "You haven't searched for any term"
+        
+        return render(request, 'search.html',{"message":message})
